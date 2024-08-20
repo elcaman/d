69bb61ecbd8f1a3e8af12c3b1ee5e0985e3d0812
nberez <nikol0880@outlook.com>
2024-08-20 12:33:19 +0300

    add files

--- src/test/java/ru/netology/rest/MobileBankApiTestV2.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV2 {
     @Test
